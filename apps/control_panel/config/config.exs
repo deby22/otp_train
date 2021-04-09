f3b9fa7245a5b52f49114482929a0bb0e580723d
ddeby <dawid.deby@gmail.com>
2021-04-09 12:23:53 +0200

    change format and log backend

--- apps/control_panel/config/config.exs
+++ apps/control_panel/config/config.exs
@@ -6,12 +6,21 @@ config :railroad,
   api_url: "http://localhost:4000/api/v2"
 
 config :logger,
-  backends: [:console, {LoggerFileBackend, :error_log}]
+  backends: [
+    :console,
+    # {LoggerFileBackend, :default_log},
+    {LoggerFileBackend, :panel_log}
+  ]
 
-config :logger, :error_log,
+# config :logger, :default_log,
+#   path: "data.log",
+#   format: "$time [$metadata] $message\n",
+#   metadata: [:speed, :station]
+
+config :logger, :panel_log,
   path: "data.log",
-  level: :debug,
-  format: "$time [$metadata] $message\n",
-  metadata: [:speed, :station]
+  format: "$time [$level] [$metadata] $message\n",
+  metadata: [:speed, :station],
+  metadata_filter: [application: :control_panel]
 
 import_config "#{Mix.env()}.exs"
